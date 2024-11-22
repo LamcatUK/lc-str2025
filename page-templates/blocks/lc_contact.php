ce9d8d4d3bf1fc27eb9044be49da5b0be1146d5c
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-22 13:13:08 +0000

    footer top margin fix

---
+++ page-templates/blocks/lc_contact.php
@@ -0,0 +1,14 @@
+<section class="contact py-5">
+    <div class="container-xl">
+        <div class="row g-5">
+            <div class="col-md-6">
+                <h2 class="h3">Direct</h2>
+                <?= do_shortcode('[contact_phone]') ?>
+                <?= do_shortcode('[contact_email]') ?>
+            </div>
+            <div class="col-md-6">
+                FORM
+            </div>
+        </div>
+    </div>
+</section>
