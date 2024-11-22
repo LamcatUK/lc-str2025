fa26ac9af16a7fe61c55c32c74a0d5c8caef8f51
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-22 13:38:42 +0000

    contact

--- page-templates/blocks/lc_contact.php
+++ page-templates/blocks/lc_contact.php
@@ -1,13 +1,21 @@
 <section class="contact py-5">
     <div class="container-xl">
         <div class="row g-5">
-            <div class="col-md-6">
-                <h2 class="h3">Direct</h2>
-                <?= do_shortcode('[contact_phone]') ?>
-                <?= do_shortcode('[contact_email]') ?>
+            <div class="col-lg-5">
+                <h2 class="h3">Contact us Direct</h2>
+                <ul class="fa-ul mb-4">
+                    <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <?= do_shortcode('[contact_phone]') ?></li>
+                    <li><span class="fa-li"><i class="fa-solid fa-paper-plane"></i></span> <?= do_shortcode('[contact_email]') ?></li>
+                </ul>
+                <h2 class="h4">Connect on Social Media</h2>
+                <div class="mb-4">
+                    <?= do_shortcode('[social_icons]') ?>
+                </div>
+                <?= do_shortcode('[lc_open_ajax]') ?>
             </div>
-            <div class="col-md-6">
-                FORM
+            <div class="col-lg-7">
+                <h2 class="h3">Send a Message</h2>
+                <?= do_shortcode("[contact-form-7 id='" . get_field('form_id') . "']") ?>
             </div>
         </div>
     </div>
