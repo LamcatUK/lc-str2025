30722c0e010a67f3d57d3081880a8007ea7a41ef
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-23 11:27:37 +0000

    Update latest_posts.php

--- page-templates/blocks-old/latest_posts.php
+++ page-templates/blocks-old/latest_posts.php
@@ -2,7 +2,7 @@
 <section class="py-4 mb-4 bg--sand-100">
     <div class="container">
         <h2 class="mb-4">Stormcatcher Resources</h2>
-        <?=latest_posts()?>
+        LATEST POSTS HERE
         <div class="text-center"><a href="/resources/" class="btn-orange">All Resources</a></div>
     </div>
 </section>
