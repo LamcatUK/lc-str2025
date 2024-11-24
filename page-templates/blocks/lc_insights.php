2938ef59792dcc2b35f92643ba99ea70f63911ec
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-24 14:10:22 +0000

    insights cards

--- page-templates/blocks/lc_insights.php
+++ page-templates/blocks/lc_insights.php
@@ -8,7 +8,7 @@ $background = get_field('bg_colour') ?: 'white';
             <h2>Stormcatcher <span>Insights</span></h2>
             <div class="line"></div>
         </div>
-        <div class="insights__grid mb-4">
+        <div class="insights__grid mb-5">
             <?php
             $q = new WP_Query(array(
                 'post_type' => 'post',
@@ -20,21 +20,37 @@ $background = get_field('bg_colour') ?: 'white';
 
             while ($q->have_posts()) {
                 $q->the_post();
+                $post_categories = get_the_category();
             ?>
                 <a class="insights__card" href="<?= get_the_permalink() ?>" data-aos="fadein" data-aos-delay="<?= $c ?>">
-                    <div class="insights__image">
-                        <?= get_the_post_thumbnail(get_the_ID(), 'large') ?>
-                    </div>
-                    <h3><?= get_the_title() ?></h3>
-                    <?php
-                    if ($first == true) {
-                    ?>
-                        <p class="fs-300"><?= wp_trim_words(get_the_content(), $length) ?>
+                    <div>
+                        <div class="insights__image">
+                            <?= get_the_post_thumbnail(get_the_ID(), 'large') ?>
+                        </div>
+                        <h3><?= get_the_title() ?></h3>
                         <?php
-                        $first = false;
-                    }
+                        if ($first == true) {
                         ?>
-                        <div class="insights__date"><?= get_the_date() ?></div>
+                            <p class="fs-300"><?= wp_trim_words(get_the_content(), $length) ?>
+                            <?php
+                            $first = false;
+                        }
+                            ?>
+                    </div>
+                    <div class="insights__meta <?= $c ?>">
+                        <div class="fs-200">
+                            <?= get_the_date() ?>
+                        </div>
+                        <div class="insights__categories">
+                            <?php
+                            if ($post_categories) {
+                            ?>
+                                <span class="insights__category"><?= esc_html($post_categories[0]->name) ?></span>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </a>
             <?php
                 $c += 100;
