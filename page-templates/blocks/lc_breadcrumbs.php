76c332c2a3de977d9df61691c68663e05e15a26e
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-11-24 17:03:27 +0000

    Update lc_breadcrumbs.php

--- page-templates/blocks/lc_breadcrumbs.php
+++ page-templates/blocks/lc_breadcrumbs.php
@@ -1,4 +1,4 @@
-<section class="breadcrumbs container-xl">
+<section class="breadcrumbs container-xl py-4">
     <?php
     if (function_exists('yoast_breadcrumb')) {
         yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
