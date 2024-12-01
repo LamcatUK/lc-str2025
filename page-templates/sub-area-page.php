4d77eef781d2dc4d6690fdd213b8bf3cd780094d
LamcatUK <git@lamcat.co.uk>
2024-12-01 16:57:01 +0000

    Update sub-area-page.php

--- page-templates/sub-area-page.php
+++ page-templates/sub-area-page.php
@@ -8,8 +8,30 @@ get_header();
 ?>
 <main>
     <?php
-    the_content();
+    foreach ($blocks as $block) {
+        if (isset($block['blockName']) && $block['blockName'] === 'acf/lc-hero') {
+            echo render_block($block);
+            // return; // Output only the first instance of the hero block
+        }
+        if (isset($block['blockName']) && $block['blockName'] === 'acf/lc-breadcrumbs') {
+            echo render_block($block);
+            // return; // Output only the first instance of the hero block
+        }
+    }
     ?>
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-lg-3">
+                <?php
+                ?>
+            </div>
+            <div class="col-lg-9">
+                <?php
+                the_content();
+                ?>
+            </div>
+        </div>
+    </div>
 </main>
 <?php
 get_footer();
