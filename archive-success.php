c61f9d62eae87d57769c20dfb89dbe78f4e591ba
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-05 16:46:02 +0000

    successes

--- archive-success.php
+++ archive-success.php
@@ -24,44 +24,35 @@ $page_for_posts = get_option('page_for_posts');
 
 <main>
     <div class="container-xl py-5">
-        <?php if (have_posts()) { ?>
-            <div class="row">
-                <?php while (have_posts()) {
-                    the_post(); ?>
-                    <div class="col-md-6 col-lg-4 mb-4">
-                        <article id="post-<?php the_ID(); ?>" <?php post_class('card h-100'); ?>>
-                            <a href="<?php the_permalink(); ?>" class="card-img-top">
-                                <?php if (has_post_thumbnail()) { ?>
-                                    <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
-                                <?php } else { ?>
-                                    <img src="<?= get_template_directory_uri(); ?>/images/placeholder.jpg" alt="Placeholder Image" class="img-fluid">
-                                <?php } ?>
-                            </a>
-                            <div class="card-body">
-                                <h2 class="card-title h5">
-                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                </h2>
-                                <p class="card-text">
-                                    <?php echo wp_trim_words(get_the_excerpt(), 20); ?>
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <a href="<?php the_permalink(); ?>" class="button button-secondary">Read More</a>
-                            </div>
-                        </article>
-                    </div>
-                <?php } ?>
-            </div>
+        <?php
+        if (have_posts()) {
+            while (have_posts()) {
+                the_post();
+        ?>
+                <article id="post-<?php the_ID(); ?>">
+                    <a href="<?= get_the_permalink() ?>" class="card">
+                        <h2 class="card-title h5"><?= get_the_title() ?></h2>
+                        <p class="card-text">
+                            <?= wp_trim_words(get_the_excerpt(), 20) ?>
+                        </p>
+                    </a>
+                </article>
+            <?php
+            } ?>
 
             <div class="pagination py-4">
                 <?= understrap_pagination(); ?>
             </div>
 
-        <?php } else { ?>
+        <?php
+        } else {
+        ?>
             <div class="alert alert-warning">
                 <p>No successes found.</p>
             </div>
-        <?php } ?>
+        <?php
+        }
+        ?>
     </div>
 </main>
 
