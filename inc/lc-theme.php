e3b95643812f108cfa4bdf216fa98dd968e6d169
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-05 17:18:46 +0000

    Update lc-theme.php

--- inc/lc-theme.php
+++ inc/lc-theme.php
@@ -307,18 +307,22 @@ function add_expertise_breadcrumb($links)
 // add_filter('wpseo_breadcrumb_links', 'add_success_cpt_to_yoast_breadcrumbs');
 
 add_filter('nav_menu_css_class', function ($classes, $item, $args, $depth) {
-    // Check if we're on the "Success" archive
+    // Check if we're on the "Success" archive or a single "Success" post
     if (is_post_type_archive('success') || is_singular('success')) {
         // Get the blog page ID
         $blog_page_id = get_option('page_for_posts');
 
-        // Remove current_page_parent from the blog menu item
+        // Forcefully remove current_page_parent from the blog menu item
         if (isset($item->object_id) && $item->object_id == $blog_page_id) {
-            $classes = array_diff($classes, ['current_page_parent']);
+            $classes = array_filter($classes, function ($class) {
+                return $class !== 'current_page_parent';
+            });
         }
     }
+
     return $classes;
 }, 10, 4);
+
 
 
 function get_sibling_pages_with_sidebar_template($post_id)
