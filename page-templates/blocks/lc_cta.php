e2e2f220c5bdd98be187770363ae53da6cf36682
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-05 17:50:12 +0000

    Update lc_cta.php

--- page-templates/blocks/lc_cta.php
+++ page-templates/blocks/lc_cta.php
@@ -1,10 +1,10 @@
 <section class="cta">
     <div class="container-xl">
         <div class="bg-primary-400 text-white text-center">
-            <div class="h3"><?= get_field('title') ?></h2>
-                <div><?= get_field('content') ?></div>
-                <a href="tel:<?= parse_phone(get_field('contact_phone', 'option')) ?>" class="button button-primary"><i class="fas fa-phone"></i> Call</a>
-                <a href="mailto:<?= get_field('contact_email', 'option') ?>" class="button button-primary"><i class="fas fa-paper-plane"></i> Email</a>
-            </div>
+            <div class="h3"><?= get_field('title') ?></div>
+            <div><?= get_field('content') ?></div>
+            <a href="tel:<?= parse_phone(get_field('contact_phone', 'option')) ?>" class="button button-primary"><i class="fas fa-phone"></i> Call</a>
+            <a href="mailto:<?= get_field('contact_email', 'option') ?>" class="button button-primary"><i class="fas fa-paper-plane"></i> Email</a>
         </div>
+    </div>
 </section>
