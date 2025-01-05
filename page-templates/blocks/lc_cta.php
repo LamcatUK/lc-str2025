2a373caf48d47886147b3a7454a075bef9d2faed
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-05 17:54:47 +0000

    Update lc_cta.php

--- page-templates/blocks/lc_cta.php
+++ page-templates/blocks/lc_cta.php
@@ -1,8 +1,23 @@
 <section class="cta">
     <div class="container-xl">
         <div class="bg-primary-400 text-white text-center p-4">
-            <div class="h3"><?= get_field('title') ?></div>
-            <div><?= get_field('content') ?></div>
+            <?php
+            if (get_field('pre_title') ?? null) {
+            ?>
+                <div class="text-uppercase fw-bold"><?= get_field('title') ?></div>
+            <?php
+            }
+            if (get_field('title') ?? null) {
+            ?>
+                <div class="h3"><?= get_field('title') ?></div>
+            <?php
+            }
+            if (get_field('content') ?? null) {
+            ?>
+                <div><?= get_field('content') ?></div>
+            <?php
+            }
+            ?>
             <a href="tel:<?= parse_phone(get_field('contact_phone', 'option')) ?>" class="button button-primary"><i class="fas fa-phone"></i> Call</a>
             <a href="mailto:<?= get_field('contact_email', 'option') ?>" class="button button-primary"><i class="fas fa-paper-plane"></i> Email</a>
         </div>
