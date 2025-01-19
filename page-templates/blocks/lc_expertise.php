224d8074600641ee23483635d9afc73e1fb5e286
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-19 16:14:00 +0000

    Update lc_expertise.php

--- page-templates/blocks/lc_expertise.php
+++ page-templates/blocks/lc_expertise.php
@@ -57,7 +57,7 @@
                 type: 'loop',
                 autoplay: true,
                 interval: 3000,
-                perPage: 1,
+                perPage: 3,
                 perMove: 1,
                 gap: '1px', // Add gap between slides
                 pagination: false, // Disable default pagination
