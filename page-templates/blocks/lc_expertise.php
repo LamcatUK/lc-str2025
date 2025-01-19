cd295e79b8e183577245cbd714bad78d85e234ef
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-19 16:18:20 +0000

    Update lc_expertise.php

--- page-templates/blocks/lc_expertise.php
+++ page-templates/blocks/lc_expertise.php
@@ -1,12 +1,12 @@
 <section class="expertise bg-grey-100 py-6">
-    <div class="container-xl">
+    <div class="container-xl" data-aos="fadein">
         <div class="row g-5">
-            <div class="col-lg-3" data-aos="fadein">
+            <div class="col-lg-3">
                 <h2 class="fancy">Our<br><span>Expertise</span></h2>
                 <p><?= get_field('intro') ?></p>
                 <a href="/expertise/" class="button button-primary">All Practice Areas</a>
             </div>
-            <div class="col-lg-9 expertise__slider" data-aos="fadein">
+            <div class="col-lg-9 expertise__slider">
                 <div class="splide" id="expertiseSplide">
                     <div class="splide__track">
                         <ul class="splide__list">
