8b20bbebe6ef5ce675861fbbab4b80bc21bf1cf8
ChillibyteUK <git@lamcat.co.uk>
2025-02-10 19:26:28 +0000

    Update header.php

--- header.php
+++ header.php
@@ -119,7 +119,7 @@ defined('ABSPATH') || exit;
     ?>
             <!-- Google Tag Manager (noscript) -->
             <noscript><iframe
-                    src="https://www.googletagmanager.com/ns.html?id=<?= get_field('ga_property', 'options') ?>"
+                    src="https://www.googletagmanager.com/ns.html?id=<?= get_field('gtm_property', 'options') ?>"
                     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <!-- End Google Tag Manager (noscript) -->
     <?php
