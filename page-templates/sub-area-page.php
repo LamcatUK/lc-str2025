018411a27be849e23ebc4e559b51d78de297a85e
LamcatUK <git@lamcat.co.uk>
2025-05-26 18:18:34 +0100

    page and subpage bio

--- page-templates/sub-area-page.php
+++ page-templates/sub-area-page.php
@@ -58,6 +58,16 @@ $blocks = parse_blocks($content);
                 echo render_block($block);
             }
         }
+        
+        if ( has_category() ) {
+            $categories = get_the_category();
+            if ( ! empty( $categories ) ) {
+                $first_category = $categories[0]; // This is a WP_Term object.
+                echo '<div class="mt-4 mb-5">';
+                echo wp_kses_post( phil_bio( $first_category->slug ) );
+                echo '</div>';
+            }
+        }
         ?>
     </div>
 </main>
--- page.php
+++ page.php
@@ -8,6 +8,16 @@ get_header();
 <main>
     <?php
     the_content();
+
+    if ( has_category() ) {
+        $categories = get_the_category();
+        if ( ! empty( $categories ) ) {
+            $first_category = $categories[0]; // This is a WP_Term object.
+            echo '<div class="mt-4">';
+            echo wp_kses_post( phil_bio( $first_category->slug ) );
+            echo '</div>';
+        }
+    }
     ?>
 </main>
 <?php
