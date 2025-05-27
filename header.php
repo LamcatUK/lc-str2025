3d793fe2eee86910f390217b4c49924b044cfc72
LamcatUK <git@lamcat.co.uk>
2025-05-27 18:03:13 +0100

    gtm

--- header.php
+++ header.php
@@ -13,6 +13,33 @@ defined( 'ABSPATH' ) || exit;
 <html <?php language_attributes(); ?>>
 
 <head>
+    <?php
+    if ( get_field( 'gtm_property', 'option' ) ) {
+        if ( ! is_user_logged_in() ) {
+            echo '<meta name="gtm-tag" content="' . esc_attr( get_field( 'gtm_property', 'option' ) ) . '" />';
+            ?>
+            <!-- Google Tag Manager -->
+            <script>
+                (function(w, d, s, l, i) {
+                    w[l] = w[l] || [];
+                    w[l].push({
+                        'gtm.start': new Date().getTime(),
+                        event: 'gtm.js'
+                    });
+                    var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s),
+                        dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true;
+                    j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', '<?= esc_attr( get_field( 'gtm_property', 'option' ) ); ?>');
+            </script>
+            <!-- End Google Tag Manager -->
+            <?php
+        }
+    }
+    ?>
     <meta
         charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -63,31 +90,6 @@ defined( 'ABSPATH' ) || exit;
     ?>
     <?php wp_head(); ?>
     <?php
-    if ( get_field( 'gtm_property', 'option' ) ) {
-        if ( ! is_user_logged_in() ) {
-            echo '<meta name="gtm-tag" content="' . esc_attr( get_field( 'gtm_property', 'option' ) ) . '" />';
-            ?>
-            <!-- Google Tag Manager -->
-            <script>
-                (function(w, d, s, l, i) {
-                    w[l] = w[l] || [];
-                    w[l].push({
-                        'gtm.start': new Date().getTime(),
-                        event: 'gtm.js'
-                    });
-                    var f = d.getElementsByTagName(s)[0],
-                        j = d.createElement(s),
-                        dl = l != 'dataLayer' ? '&l=' + l : '';
-                    j.async = true;
-                    j.src =
-                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                    f.parentNode.insertBefore(j, f);
-                })(window, document, 'script', 'dataLayer', '<?= esc_attr( get_field( 'gtm_property', 'option' ) ); ?>');
-            </script>
-            <!-- End Google Tag Manager -->
-            <?php
-        }
-    }
     if ( get_field( 'ga_property', 'option' ) ) {
         if ( ! is_user_logged_in() ) {
             ?>
