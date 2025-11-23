c63e7633c8da09661d091635f66c3103bb874b96
LamcatUK <git@lamcat.co.uk>
2025-11-23 16:46:49 +0000

    hero badge

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -46,8 +46,10 @@ $c = is_front_page() === 1 ? 'home_hero' : '';
         }
         ?>
             </div>
-            <div class="col-md-4 my-auto" data-aos="fadein" data-aos-delay="<?= esc_attr( $c + 200 ); ?>" style="min-height:80px;">
-                <?= do_shortcode( '[trustindex data-widget-id=a23e05058af5912541863bba2b3]' ); ?>
+            <div class="col-md-4 my-auto" data-aos="fadein" data-aos-delay="<?= esc_attr( $c + 200 ); ?>">
+                <div class="badge mt-4" style="min-height:80px;">
+                    <?= do_shortcode( '[trustindex data-widget-id=a23e05058af5912541863bba2b3]' ); ?>
+                </div>
             </div>
         </div>
     </div>
